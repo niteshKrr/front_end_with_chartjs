4b7b50fade675b352027418339d0dd4ddeac04e8
Niteshkumar <nkumar35101@gmail.com>
2022-11-09 12:27:39 +0530

    some of the chart examples created

--- components/chart/AreaChart.jsx
+++ components/chart/AreaChart.jsx
@@ -3,20 +3,24 @@ import {
   Chart as ChartJS,
   CategoryScale,
   LinearScale,
-  BarElement,
+  PointElement,
+  LineElement,
   Title,
   Tooltip,
+  Filler,
   Legend,
 } from 'chart.js';
-import { Bar } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 import { faker } from "@faker-js/faker";
 
 ChartJS.register(
   CategoryScale,
   LinearScale,
-  BarElement,
+  PointElement,
+  LineElement,
   Title,
   Tooltip,
+  Filler,
   Legend
 );
 
@@ -28,7 +32,7 @@ export const options = {
     },
     title: {
       display: true,
-      text: 'Chart.js Bar Chart',
+      text: 'Chart.js Line Chart',
     },
   },
 };
@@ -39,13 +43,10 @@ export const data = {
   labels,
   datasets: [
     {
-      label: 'Dataset 1',
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-    {
+      fill: true,
       label: 'Dataset 2',
       data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      borderColor: 'rgb(53, 162, 235)',
       backgroundColor: 'rgba(53, 162, 235, 0.5)',
     },
   ],
@@ -53,9 +54,9 @@ export const data = {
 
 export default function App() {
   return (
-    <div style={{heigh:400 , width:600}} className="container mx-auto my-32">
-      <h2 style={{textAlign: "center"}}>Vertical Bar Chart</h2>
-      <Bar options={options} data={data} />
+    <div style={{ heigh: 400, width: 600 }} className="container mx-auto my-32">
+      <h1 style={{ textAlign: "center" }}>Area Chart</h1>
+      <Line options={options} data={data} />
     </div>
-  ) 
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,29 @@
 import React from "react";
-import Vertical_barChart from "../components/chart/Vertical_barChat";
+import Vertical_barChart from "../components/chart/Vertical_barChart";
+import Horizontal_barChart from "../components/chart/Horizontal_barChart";
+import AreaChart from "../components/chart/AreaChart";
+import PieChart from "../components/chart/PieChart";
+import DoughnutChart from "../components/chart/DoughnutChart";
 
 const index = () => {
   return (
-    <div>
-      <Vertical_barChart />
-    </div>
+    <>
+      <div>
+        <Vertical_barChart />
+      </div>
+      <div>
+        <Horizontal_barChart />
+      </div>
+      <div>
+        <AreaChart />
+      </div>
+      <div>
+        <PieChart />
+      </div>
+      <div>
+        <DoughnutChart />
+      </div>
+    </>
   );
 };
 
